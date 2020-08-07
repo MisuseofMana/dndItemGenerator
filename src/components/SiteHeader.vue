f1a51e5b673233a1db9b5bffe51c0995beaef1d0
Sean <contact@seanyager.com>
2020-08-07 17:57:54 -0500

    got it done, now were done.

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Shib and Daveys Gerblin Treasuer</h1>
+        <ul>
+            <li>Item Generator</li>
+            <li>Character Generator</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SiteHeader',
+    }
+</script>
+
+<style scoped>
+div {
+    background:violet;
+}
+</style>
