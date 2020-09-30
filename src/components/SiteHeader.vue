623ef16cd00cdb9f1fb18dac032719a81ca8f78c
Sean Yager <contact@seanyager.com>
2020-09-29 20:22:46 -0500

    overhaul to site Header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,21 +1,53 @@
 <template>
-    <div>
+    <header>
+
         <h1>Shib and Daveys Gerblin Treasuer</h1>
-        <ul>
-            <li>Item Generator</li>
-            <li>Character Generator</li>
-        </ul>
-    </div>
+        
+        <section>
+            <button @click="$emit('change-page', 'item')">Item Generator</button>
+            <button @click="$emit('change-page', 'char')">Character Generator</button>
+        </section>
+        <img src="@/assets/imgs/banner.png" alt="">
+    </header>
 </template>
 
 <script>
     export default {
         name: 'SiteHeader',
+        methods: {
+            
+        }
     }
 </script>
 
 <style scoped>
-div {
-    background:violet;
+h1 {
+    padding: 10px 0px 0px 10px;
+    margin:0px;
+    color: goldenrod;
+}
+
+section button {
+    padding:10px;
+    
+}
+
+section {
+    width:600px;
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+}
+
+header {
+    background:darkslateblue;
+    height:100px;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+}
+
+header img {
+    height: 100px;
 }
 </style>
