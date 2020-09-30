3de4537ac5e9520d18863f8a69298abc971a6984
Sean Yager <contact@seanyager.com>
2020-09-29 20:21:33 -0500

    Huge overhaul to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="app">
-    <site-header/>
-    <item-generator/>
+    <site-header @change-page="togglePage($event)"/>
+    <char-generator v-if="page==='char'"/>
+    <item-generator v-else-if="page==='item'"/>
+    <readout-block/>
   </div>
 </template>
 
 <script>
 import SiteHeader from './components/SiteHeader'
 import ItemGenerator from './components/ItemGenerator'
+import CharGenerator from './components/CharGenerator'
+import ReadoutBlock from './components/ReadoutBlock'
+
 import './assets/styles/global.css';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      page: 'item',
+    }
+  },
+  methods: {
+    togglePage(newPage) {
+      this.page = newPage
+    }
+  },
   components: {
     SiteHeader,
     ItemGenerator,
+    CharGenerator,
+    ReadoutBlock,
   }
 }
 
