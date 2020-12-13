4d434da5430101aae756f460c4305beb922c4a58
Sean Yager <contact@seanyager.com>
2020-12-13 17:28:43 -0600

    removed old issues

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@
     <!-- SiteHeader button click emits change page event and payload -->
     <!-- We run togglePage function and pass in the argument $event as the payload -->
     <site-header @change-page="togglePage($event)"/>
-    <char-generator v-if="activePageIs==='char'"/>
-    <item-generator v-else-if="activePageIs==='item'"/>
-    <readout-block/>
+    <item-generator v-if="activePageIs==='item'"/>
   </div>
 </template>
 
@@ -14,8 +12,6 @@
 // import and declare our vue component
 import SiteHeader from './components/SiteHeader'
 import ItemGenerator from './components/ItemGenerator'
-import CharGenerator from './components/CharGenerator'
-import ReadoutBlock from './components/ReadoutBlock'
 
 // importing stylesheet from assets
 import './assets/styles/global.css';
@@ -27,6 +23,7 @@ export default {
     return {
       //default set page state to item.
       activePageIs: 'item',
+      selectedBoxes:[],
     }
   },
   methods: {
@@ -39,15 +36,12 @@ export default {
     // App file's references
     SiteHeader,
     ItemGenerator,
-    CharGenerator,
-    ReadoutBlock,
   }
 }
 
 </script>
 
 <style>
-
 </style>
 
 
